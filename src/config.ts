8d79ef568d0e6a8bf31cadd5519f60b2d1ea7b02
CometBlazer <jinghaowang1997@gmail.com>
2025-06-19 15:09:27 -0400

    Update website configuration in config.ts with new branding details, including name, base URL, slogan, and description for Haloway. Enable profile creation step.

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,12 @@
 import type { Provider } from '@supabase/supabase-js';
 
-export const WebsiteName: string = 'SaaS Kit';
+export const WebsiteName: string = "Haloway"
+export const WebsiteBaseUrl: string = "https://haloway.pages.dev"
+export const WebsiteSlogan: string =
+  "the ULTIMATE college essay writing platform"
+export const WebsiteDescription: string =
+  "Haloway is a college essay writing platform that helps students draft, organize, and finalize their essays with ease. With beautiful UI and draft-management features, Haloway aims to replace Google Docs as the go-to tool for college essay writing."
+export const CreateProfileStep: boolean = true
 
 /* You'll need to configure your providers in
 your Supabase project settings `/supabase/config.toml` */
