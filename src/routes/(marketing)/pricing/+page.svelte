50548a949a6884239a2736506f954e5a582ebd21
Alex <jinghaowang1997@gmail.com>
2025-06-24 12:12:47 -0400

    feat: add pricing page with server-side data loading and marketing content

---
+++ src/routes/(marketing)/pricing/+page.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import * as Section from '$lib/components/landing/section';
+	// import * as Card from '$lib/components/ui/card';
+	// import Features from './components/sections/features/features.svelte';
+	// import HeroSection from './components/sections/hero.svelte';
+	// import LogosCloud from './components/sections/logos-cloud.svelte';
+	import Pricing from '../components/sections/pricing.svelte';
+	// import Testimonials from './components/sections/testimonials.svelte';
+
+	export let data;
+
+	const { prices } = data;
+
+	// This page is for marketing purposes, so it does not need to be server-rendered.
+	export const ssr = false;
+</script>
+
+<Section.Root anchor="pricing">
+	<Section.Header>
+		<Section.Title>Pricing</Section.Title>
+		<Section.Description class="text-balance">
+			Currently we support subscription based pricing out of the box. However,
+			you can extend the boilerplate to support one-time or custom pricing
+			models.
+		</Section.Description>
+	</Section.Header>
+	<Pricing {prices} />
+</Section.Root>
