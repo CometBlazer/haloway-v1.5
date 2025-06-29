cc60c962dab488f75549054cd620ff3e515ce4d3
Alex <jinghaowang1997@gmail.com>
2025-06-28 21:37:54 -0400

    fix: update meta tags to use dynamic values for title and description, improving SEO and maintainability

--- src/routes/(marketing)/meta-tags.svelte
+++ src/routes/(marketing)/meta-tags.svelte
@@ -1,16 +1,18 @@
 <script lang="ts">
+	import {
+		WebsiteBaseUrl,
+		WebsiteDescription,
+		WebsiteName,
+	} from '../../config';
 </script>
 
 <svelte:head>
-	<meta name="title" content="SaaS Kit" />
-	<meta
-		name="description"
-		content="An open-source SaaS starter-kit built with SvelteKit, Supabase, TailwindCSS, Stripe, and shadcn-svelte. Host and scale up to 50,000 customers for free on Supabase and Cloudflare Workers."
-	/>
+	<meta name="title" content={WebsiteName} />
+	<meta name="description" content={WebsiteDescription} />
 	<meta
 		name="keywords"
-		content="saas, template, boilerplate, svelte, svelte-kit, supabase, typescript, postgres, postgresql, shadcn-ui, shadcn-svelte, tailwindcss, stripe, open source, free"
+		content="college essay writing, college essays, college essay help, college essay tips, college apps, college application, essay examples, essay writing service"
 	/>
 	<meta name="robots" content="index, follow" />
-	<link rel="canonical" href="https://saaskit.live" />
+	<link rel="canonical" href={WebsiteBaseUrl} />
 </svelte:head>
